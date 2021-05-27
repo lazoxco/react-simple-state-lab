9d04bc81812bef6d850e422ecf8f9cb451ffc52d
github username <github email address>
2021-05-27 11:01:52 -0400

    Added state to cell component and rendered div.

--- src/Cell.js
+++ src/Cell.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
 
 export default class Cell extends Component {
-  
+
+  constructor() {
+    super()
+    this.state = {color: this.prop}
+  }
+
+  render() {
+    return <div className="cell" style={{backgroundColor: this.state.color}}></div>
+  }
 }
